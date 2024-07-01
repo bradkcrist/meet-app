4ebefb0139385e3705ccd5363d516074cf9d1b27
bradkcrist <bradkcrist08@gmail.com>
2024-07-01 04:53:13 -0600

    Created new components to test using jest.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import CitySearch from './component/CitySearch';
+import EventList from './component/EventList';
 import './App.css';
+import NumberOfEvents from './component/NumberOfEvents';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <CitySearch />
+      <EventList />
+      <NumberOfEvents />
     </div>
   );
-}
-
+};
 export default App;
