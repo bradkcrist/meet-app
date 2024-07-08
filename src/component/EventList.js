5bf9455145a1324a6ea5f73c4b0db944133f110e
bradkcrist <bradkcrist08@gmail.com>
2024-07-08 04:32:16 -0600

    created public folder and updated eventlist

--- src/component/EventList.js
+++ src/component/EventList.js
@@ -1,6 +1,7 @@
 import Event from './Event';
 
-const EventList = ({ events = [] }) => {
-  return <ul id='event-list'>{events ? events.map((event) => <Event key={event.id} event={event} />) : null}</ul>;
+const EventList = ({ events }) => {
+  return <ul id='event-list'>{events ? events.map((event) => <Event event={event} key={event.id} />) : null}</ul>;
 };
+
 export default EventList;
